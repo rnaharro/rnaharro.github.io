660a34ea5d36156b6a927ee888b259560853c3cc
Jorge Moreno <alterebro@gmail.com>
2016-03-16 16:28:16 +0100

    xhr wrapper included

---
+++ frontend/scripts/app.js
@@ -0,0 +1,82 @@
+var githubUser = 'alterebro';
+var githubAPI = {
+	repos : 'https://api.github.com/users/' + githubUser + '/repos?sort=pushed',
+	user : 'https://api.github.com/users/' + githubUser
+}
+
+var data = {
+	user : githubUser,
+	repos : null,
+	user_languages : {},
+	stars : 0,
+	forks : 0,
+	watching : 0
+};
+
+var app = new Vue({
+
+	el : '#app',
+	data: data,
+
+	created: function () {
+		this.getReposData();
+	},
+
+	filters : {
+		filterdate : function(str) {
+			var _d = new Date(str);
+			var  d = {
+				d : ("0" + _d.getUTCDate()).slice (-2),
+				m : ("0" + _d.getUTCMonth()).slice (-2),
+				y : _d.getUTCFullYear(),
+				hh : ("0" + _d.getUTCHours()).slice (-2),
+				mm : ("0" + _d.getUTCMinutes()).slice (-2)
+			};
+			return (d.d + '.' + d.m + '.' + d.y + ' @' + d.hh + ':' + d.mm);
+		}
+	},
+
+	methods : {
+
+		getLanguages : function(index, url) {
+
+			data.repos[index].main_language = data.repos[index].language;
+
+			atomic.get(url).success(function (d, x) {
+				var res = d;
+				var repo_langs = [];
+				for ( var i in res ) {
+					repo_langs.push(i);
+					if ( i in data.user_languages ) {
+						Vue.set(data.user_languages, i, (data.user_languages[i] + res[i]));
+					} else {
+						Vue.set(data.user_languages, i, res[i]);
+					}
+				}
+				data.repos[index].language = repo_langs.join(', ');
+
+			})
+			.error(function () {})
+			.always(function () {});
+		},
+
+		getReposData : function() {
+
+			var self = this;
+
+			atomic.get(githubAPI.repos).success(function (d, x) {
+				var reposData = d;
+				data.repos = reposData;
+				for (var i=0; i<reposData.length; i++) {
+					data.stars += reposData[i].stargazers_count;
+					data.forks += reposData[i].forks_count;
+					data.watching += reposData[i].watchers_count;
+
+					self.getLanguages(i, reposData[i].languages_url);
+				}
+			})
+			.error(function () {})
+			.always(function () {});
+		}
+	}
+});
